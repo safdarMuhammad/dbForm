884e949a851d9735f15cc6c7f24d31ab0c54e496
Safdar <safdarsssmuhammad@gmail.com>
2023-11-22 09:59:55 +0530

    first commit

---
+++ api.php
@@ -0,0 +1,10 @@
+<?php
+$dbName = 'dbForm';
+$host = 'localhost';
+$pass = "";
+
+
+$db = mysqli_connect($host, $dbName, $pass);
+
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Welcome to the Feedback submittion for Admission in VIET</h1>
+    <form action="/api.php" method='get'>
+        <input type="text" placeholder="Enter Your Name">
+        <input type="email" placeholder="Enter your Email">
+        <input type="number" placeholder="Enter Your Enroll No.">
+        <input type="textarea" placeholder="Type your feedback">
+        <button type="submit">SUBMIT FEEDBACK</button>
+    </form>
+</body>
+
+</html>
